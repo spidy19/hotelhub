f4267fcc76e246b66c8f0df12065b7c0ced04acf
spy232000 <spy200023@gmail.com>
2024-04-10 18:31:16 +0530

    hello

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -58,7 +58,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             cursor-pointer
           "
         >
-          Airbnb your home
+          Hotelhub your home
         </div>
         <div 
         onClick={toggleOpen}
@@ -119,7 +119,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   onClick={() => router.push('/properties')}
                 />
                 <MenuItem 
-                  label="Airbnb your home" 
+                  label="Hotelhub your home" 
                   onClick={rentModal.onOpen}
                 />
                 <hr />
